0545d14a0b35c811a832d29dea5d6586b0f6785c
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2022-01-28 02:00:15 +0700

    testing get route

--- vapor/HelloVapor/Sources/App/routes.swift
+++ vapor/HelloVapor/Sources/App/routes.swift
@@ -4,8 +4,22 @@ func routes(_ app: Application) throws {
     app.get { req in
         return "It works!"
     }
-
-    app.get("hello") { req -> String in
+    
+    //localhost:8080/hi
+    app.get("hi") { req -> String in
         return "Hello, world!"
+    }
+    
+    //localhost:8080/test/vapor
+    app.get("test", "vapor") { req -> String in
+        return "Hello Vapor!"
+    }
+    
+    //localhost:8080/hello/tommy
+    app.get("hello", ":name") { req -> String in
+        guard let name = req.parameters.get("name") else {
+            throw Abort(.internalServerError)
+        }
+        return "Hello, \(name)!"
     }
 }
--- vapor/HelloVapor/Tests/AppTests/AppTests.swift
+++ vapor/HelloVapor/Tests/AppTests/AppTests.swift
@@ -7,7 +7,7 @@ final class AppTests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
 
-        try app.test(.GET, "hello", afterResponse: { res in
+        try app.test(.GET, "hi", afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
             XCTAssertEqual(res.body.string, "Hello, world!")
         })
