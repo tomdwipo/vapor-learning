2b3739db1a1737dfc3d5a23ecd25fd792b0a49fe
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2022-01-28 02:22:12 +0700

    add some tests

--- vapor/HelloVapor/Tests/AppTests/AppTests.swift
+++ vapor/HelloVapor/Tests/AppTests/AppTests.swift
@@ -2,8 +2,19 @@
 import XCTVapor
 
 final class AppTests: XCTestCase {
-    func testHelloWorld() throws {
-        let app = Application(.testing)
+    var app: Application!
+  
+    override func setUp() {
+        super.setUp()
+        app = Application(.testing)
+    }
+    
+    override func tearDown() {
+        app = nil
+        super.tearDown()
+    }
+ 
+    func test_get_hi_shouldReturnHelloWorld() throws {
         defer { app.shutdown() }
         try configure(app)
 
@@ -12,4 +23,35 @@ final class AppTests: XCTestCase {
             XCTAssertEqual(res.body.string, "Hello, world!")
         })
     }
+    
+    func test_get_test_vapor_shouldReturnHellopVapor() throws {
+        defer { app.shutdown() }
+        try configure(app)
+
+        try app.test(.GET, "test/vapor", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "Hello Vapor!")
+        })
+    }
+    
+    func test_get_hello_tommy_shouldReturnHelloTommy() throws {
+        defer { app.shutdown() }
+        try configure(app)
+
+        try app.test(.GET, "hello/tommy", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "Hello, tommy!")
+        })
+    }
+    
+    func test_get_hello_shouldReturnStatusNotFound() throws {
+        defer { app.shutdown() }
+        try configure(app)
+
+        try app.test(.GET, "hello/", afterResponse: { res in
+            XCTAssertEqual(res.status, .notFound)
+        })
+    }
+    
+    
 }
